a46a1a490c4dc80c9fb3fdfa9fc44b8571fd23c2
Darius Fang <dariusfang1@gmail.com>
2025-02-04 13:40:45 -0700

    added some shadUI components
    added button, card, input

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import fetchUsers from "./firebase/functions/fetchUsers";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [userData, setUsers] = useState([]);
@@ -20,6 +21,7 @@ function App() {
   return (
     <>
       <h1>Feedback Cubbies</h1>
+      <Button>Click me</Button>
     </>
   );
 }
