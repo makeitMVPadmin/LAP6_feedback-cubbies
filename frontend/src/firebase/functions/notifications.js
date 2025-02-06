8e6dfbc77a62d9f6b116cfe4d16c36b0ce5f4813
rouenemedina <rouenemedina@gmail.com>
2025-02-06 08:06:13 -0700

    added createNotification and getNotifications functions to create and read notifications, created filter functions such as getUnreadNotification and markNotificationAsRead to get only the unread notifications and set readStatus to true

--- frontend/src/firebase/functions/notifications.js
+++ frontend/src/firebase/functions/notifications.js
@@ -1,7 +1,16 @@
 import { db } from "../firebase.js";
-import { collection, addDoc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  serverTimestamp,
+  query,
+  where,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 
-//table: 
+// Data Structure:
 //id - notification id
 //userId - sender
 //portfolioId - receiver
@@ -12,7 +21,89 @@ import { collection, addDoc, getDocs } from "firebase/firestore";
 //updatedAt
 
 //Create a new notification
+const createNotification = async (params) => {
+  try {
+    const docRef = await addDoc(collection(db, "notification"), {
+      userId: params.userId,
+      portfolioId: params.portfolioId,
+      feedbackId: params.feedbackId,
+      message: params.message,
+      readStatus: false,
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+    });
+    return docRef;
+  } catch (err) {
+    console.error("Error creating notification: ", err);
+    throw new Error(`createNotification failed: ${err.message}`);
+  }
+};
 
 //Get all notifications for a user
+const getNotifications = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, "notification"));
+    const notificationArray = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    console.log(notificationArray);
+    return notificationArray;
+  } catch (err) {
+    console.error("Error getting notifications: ", err);
+    throw new Error(`getNotifications failed: ${err.message}`);
+  }
+};
 
 //Get all unread notifications for a user
+const getUnreadNotifications = async ({ userId }) => {
+  try {
+    // Filtering for unread notifications of the entire collection
+    const notificationsRef = collection(db, "notification");
+    const notifQuery = query(
+      notificationsRef,
+      where("userId", "==", userId),
+      where("readStatus", "==", false)
+    );
+
+    const querySnapshot = await getDocs(notifQuery);
+
+    // Extracting the unread notification data
+    const unreadNotifArray = [];
+    querySnapshot.forEach((doc) => {
+        unreadNotifArray.push({
+            id: doc.id,
+            ...doc.data(),
+        });
+    })
+
+    console.log(unreadNotifArray);
+    return unreadNotifArray;
+  } catch (err) {
+    console.error("Error getting notifications: ", err);
+    throw new Error(`getUnreadNotifications failed: ${err.message}`);
+  }
+};
+
+// Mark a notification as read
+const markNotificationAsRead = async ({ notificationId }) => {
+    try {
+        // Reference to a single/specific document
+        const notificationsDocRef = doc(db, "notification", notificationId);
+        await updateDoc (notificationsDocRef, {
+            readStatus: true,
+            updatedAt: serverTimestamp(),
+        });
+        return true;
+    } catch (err) {
+        console.error("Error marking notification as read: ", err);
+        throw new Error(`markNotificationAsRead failed: ${err.message}`);
+    }
+}
+
+export default {
+  createNotification,
+  getNotifications,
+  getUnreadNotifications,
+  markNotificationAsRead
+};
