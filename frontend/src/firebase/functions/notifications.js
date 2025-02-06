79b67f7b2e04609e5cc90fd2a14bb2142ca5616c
rouenemedina <rouenemedina@gmail.com>
2025-02-06 06:48:21 -0700

    created an initial setup of the  notification function with no boilerplate in firebase

--- frontend/src/firebase/functions/notifications.js
+++ frontend/src/firebase/functions/notifications.js
@@ -1,3 +1,6 @@
+import { db } from "../firebase.js";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+
 //table: 
 //id - notification id
 //userId - sender
@@ -7,3 +10,9 @@
 //readStatus
 //createdAt
 //updatedAt
+
+//Create a new notification
+
+//Get all notifications for a user
+
+//Get all unread notifications for a user
