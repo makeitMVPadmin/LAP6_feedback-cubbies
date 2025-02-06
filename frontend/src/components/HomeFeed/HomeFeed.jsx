15e25a65bf3a4527c006c1a86a3e686e56dd4c05
Maria Lukowich <mlukowich27@gmail.com>
2025-02-06 11:50:51 -0700

    functionality is done, but I can't get the shadcn to work

--- frontend/src/components/HomeFeed/HomeFeed.jsx
+++ frontend/src/components/HomeFeed/HomeFeed.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Post from "../Post/Post.jsx";
+import img1 from "/images/MockImg1.jpg"
+import img2 from "/images/MockImg2.jpg"
+import img3 from "/images/MockImg3.jpg"
+import resume from "/images/MockResume.png"
 
 // loop through the posted portfolios
 function HomeFeed() {
@@ -10,14 +14,14 @@ function HomeFeed() {
     "username": "johnSmith",
     "title": "My Portfolio",
     "description": "Brief description of my portfolio. More details.",
-    "images": ["image1.jpg", "image2.jpg", "image3.jpg"]
+    "images": [img1, img2, img3]
     },
     {
       "id": 2,
       "username": "janeDoe",
       "title": "My Resume",
       "description": "My resume, looking for feedback.",
-      "images": ["resume1.jpg"]
+      "images": [resume]
     },
   ]
 
--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -6,25 +6,54 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-
+} from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 // rendering a single posted portfolio
-function Post({ post}) {
+function Post({ post }) {
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>{post.username}</CardTitle>
-        <CardTitle>{post.title}</CardTitle>
-        <CardDescription>{post.description}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        {post.images.map((image, index) => (
-            <img key={index} src={image} alt={post.title} />
+    <CardHeader>
+      <CardTitle>{post.username}</CardTitle>
+      <CardTitle>{post.title}</CardTitle>
+      <CardDescription>{post.description}</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+          {/* Loop through the images array from the mock data */}
+          {post.images.map((image, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <img
+                      src={image}
+                      alt={`image ${index + 1} from ${post.title}`}
+                      style={{
+                        width: "350px",
+                        height: "auto",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
           ))}
-      </CardContent>
-    </Card>
-  )
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </CardContent>
+  </Card>
+  );
 }
 
-export default Post
+export default Post;
