d83bc9406d0bb4307cd97bb9c98733a3f5fb379f
Darius Fang <dariusfang1@gmail.com>
2025-02-06 12:28:41 -0700

    added nav
    added home, comments and a drop down to start

--- frontend/src/components/TopNav/TopNav.jsx
+++ frontend/src/components/TopNav/TopNav.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Link } from "react-router-dom";
 
-function TopNav() {
+function NavigationMenuDemo() {
   return (
-    <div>TopNav</div>
-  )
-}
+    <NavigationMenu>
+      <NavigationMenuList className="">
+        <NavigationMenuItem className="">
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <Link to="/">Home</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
 
-export default TopNav
+        <NavigationMenuItem className="">
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <Link to="/comments">Comments</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem className="">
+          <NavigationMenuTrigger>Notifications</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Link to="/comments">notification1</Link>
+            </NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+export default NavigationMenuDemo;
