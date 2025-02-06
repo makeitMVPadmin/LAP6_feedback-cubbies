b464dcab84ffb9ab842a8f41a1e8af4e891872a8
Maria Lukowich <mlukowich27@gmail.com>
2025-02-06 10:51:15 -0700

    creating the mock data for the homefeed

--- frontend/src/components/HomeFeed/HomeFeed.jsx
+++ frontend/src/components/HomeFeed/HomeFeed.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 
 function HomeFeed() {
+  // mock data - replace with actual data later
+  let mockPosts = [
+    {
+    "id": 1,
+    "username": "johnSmith",
+    "title": "My Portfolio",
+    "description": "Brief description of my portfolio. More details.",
+    "images": ["image1.jpg", "image2.jpg", "image3.jpg"]
+    },
+    {
+      "id": 2,
+      "username": "janeDoe",
+      "title": "My Resume",
+      "description": "My resume, looking for feedback.",
+      "images": ["resume1.jpg"]
+    },
+  ]
+
   return (
-    <div>HomeFeed</div>
+    <section className="">
+      {mockPosts.map((post) => (
+      <div key={post.id} className="">
+        <h2 className="">{post.username}</h2>
+        <h3 className="">{post.title}</h3>
+        <p className="">{post.description}</p>
+        <div className="carousel">
+          {post.images.map((image, index) => (
+            <img className="" key={index} src={image} alt={post.title} />
+          ))}
+        </div>
+      </div>
+      ))}
+    </section>
   )
 }
 
