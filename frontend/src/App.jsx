40ec4f9992ed7c6b739271b8faadb7ac149cdef4
Tanya Vezyr <tetiana.vezyr@gmail.com>
2025-02-10 13:57:28 -0700

    clean app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import HomePage from "./pages/HomePage";
 import NavigationMenuDemo from "@/components/TopNav/TopNav";
-import { Button } from "@/components/ui/button";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import FeedbackPage from "./pages/FeedbackPage";
@@ -8,23 +7,19 @@ import FeedbackPage from "./pages/FeedbackPage";
 function App() {
   return (
     <>
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <header className="bg-white shadow-md p-4">
-          <NavigationMenuDemo />
-        </header>
-        <main className="flex-grow p-4">
-          <h1>Feedback Cubbies</h1>
-          <Button>Click me</Button>
-
-          {/* Define Routes */}
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/feedback" element={<FeedbackPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+      <Router>
+        <div className="min-h-screen flex flex-col">
+          <header className="bg-white shadow-md p-4">
+            <NavigationMenuDemo />
+          </header>
+          <main className="flex-grow p-4">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/feedback" element={<FeedbackPage />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
     </>
   );
 }
