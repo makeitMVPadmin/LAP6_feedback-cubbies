a5182dcceba2dcea88639255fb42b259eff644bc
Jason Korol <ejkorol@gmail.com>
2025-02-10 15:15:57 -0500

    updates shadcn component for canary spec
    - `package.json` now uses tailwind @^4, in order for variants to work
      correctly you need to use the canary version of shadcn CLI: `npx shadcn@canary add <component>`: https://ui.shadcn.com/docs/tailwind-v4
    - updates `components.json` to disregard typescript components
    - updates all existing components using canary command

--- frontend/src/components/ui/card.jsx
+++ frontend/src/components/ui/card.jsx
@@ -2,49 +2,71 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-const Card = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("rounded-xl border bg-card text-card-foreground shadow", className)}
-    {...props} />
-))
-Card.displayName = "Card"
+function Card({
+  className,
+  ...props
+}) {
+  return (
+    (<div
+      data-slot="card"
+      className={cn("bg-card text-card-foreground rounded-xl border shadow-sm", className)}
+      {...props} />)
+  );
+}
 
-const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
-    {...props} />
-))
-CardHeader.displayName = "CardHeader"
+function CardHeader({
+  className,
+  ...props
+}) {
+  return (
+    (<div
+      data-slot="card-header"
+      className={cn("flex flex-col gap-1.5 p-6", className)}
+      {...props} />)
+  );
+}
 
-const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("font-semibold leading-none tracking-tight", className)}
-    {...props} />
-))
-CardTitle.displayName = "CardTitle"
+function CardTitle({
+  className,
+  ...props
+}) {
+  return (
+    (<div
+      data-slot="card-title"
+      className={cn("leading-none font-semibold tracking-tight", className)}
+      {...props} />)
+  );
+}
 
-const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
-    {...props} />
-))
-CardDescription.displayName = "CardDescription"
+function CardDescription({
+  className,
+  ...props
+}) {
+  return (
+    (<div
+      data-slot="card-description"
+      className={cn("text-muted-foreground text-sm", className)}
+      {...props} />)
+  );
+}
 
-const CardContent = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
-))
-CardContent.displayName = "CardContent"
+function CardContent({
+  className,
+  ...props
+}) {
+  return (<div data-slot="card-content" className={cn("p-6 pt-0", className)} {...props} />);
+}
 
-const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
-    {...props} />
-))
-CardFooter.displayName = "CardFooter"
+function CardFooter({
+  className,
+  ...props
+}) {
+  return (
+    (<div
+      data-slot="card-footer"
+      className={cn("flex items-center p-6 pt-0", className)}
+      {...props} />)
+  );
+}
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
