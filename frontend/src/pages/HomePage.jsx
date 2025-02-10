8be7225cb6af9baf6d442b00cac0cd39fb82d1f7
Darius Fang <dariusfang1@gmail.com>
2025-02-10 12:23:39 -0700

    changed pages names

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
-export default function Home() {
+import HomeFeed from "@/components/HomeFeed/HomeFeed";
+
+export default function HomePage() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page.</p>
+      <HomeFeed />
     </div>
   );
 }
