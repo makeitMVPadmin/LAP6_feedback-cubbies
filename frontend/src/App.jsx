154b7d593d025ee3798fb2dbda29a0ded6a37922
Nyakuma Wal <Nyakuma.wal@gmail.com>
2025-02-10 16:00:06 -0500

    Porfolio js file created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import fetchPortfolio from "./firebase/functions/fetchPortfolio";
 import fetchUsers from "./firebase/functions/fetchUsers";
 import { Button } from "@/components/ui/button";
 
 function App() {
   const [userData, setUsers] = useState([]);
+  const [portfolioData, setPortfolioData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const data = await fetchUsers();
         setUsers(data);
+
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        const portfolio = await fetchPortfolio();
+        setPortfolioData(portfolio);
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
       } catch (error) {
         console.error("Error fetching users:", error);
       }
@@ -22,6 +29,13 @@ function App() {
     <>
       <h1>Feedback Cubbies</h1>
       <Button>Click me</Button>
+
+      <h2>Portfolio</h2>
+      <ul>
+        {portfolioData.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
