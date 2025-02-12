2e7f5840fec8520324e2e1db015d39a627419560
rouenemedina <rouenemedina@gmail.com>
2025-02-12 14:17:32 -0700

    added comments for mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <div className="min-h-screen flex flex-col">
           <header className="bg-white shadow-md p-4">
             <NavigationMenuDemo />
-            <NotificationTabs ownerUserId="Dn8E6i4iljA5zqTY0ghu"/>
+            <NotificationTabs ownerUserId="Dn8E6i4iljA5zqTY0ghu"/>  // Replace with the actual user ID
           </header>
           <main className="flex-grow p-4">
             <Routes>
