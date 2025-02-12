c526f79c2ff044f6b5270003b193d1149e6610d4
Darius Fang <dariusfang1@gmail.com>
2025-02-12 14:35:06 -0700

    changed router to states instead

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,30 @@
+import FeedbackPage from "./pages/FeedbackPage";
 import HomePage from "./pages/HomePage";
 import NavigationMenuDemo from "@/components/TopNav/TopNav";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
-import FeedbackPage from "./pages/FeedbackPage";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "home":
+        return <HomePage />;
+      case "feedback":
+        return <FeedbackPage />;
+      default:
+        return <HomePage />;
+    }
+  };
+
   return (
-    <>
-      <Router>
-        <div className="min-h-screen flex flex-col">
-          <header className="bg-white shadow-md p-4">
-            <NavigationMenuDemo />
-          </header>
-          <main className="flex-grow p-4">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/feedback" element={<FeedbackPage />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-white shadow-md p-4">
+        <NavigationMenuDemo setCurrentPage={setCurrentPage} />
+      </header>
+      <main className="flex-grow p-4">{renderPage()}</main>
+    </div>
   );
 }
 
