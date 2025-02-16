02da8605e6dd93223a1cc5f6f79bdfabccc155cd
Maite <maitemsabaida@gmail.com>
2025-02-16 13:48:40 -0700

    Adjusting styles

--- frontend/src/components/PostModal/PostModal.jsx
+++ frontend/src/components/PostModal/PostModal.jsx
@@ -1,4 +1,5 @@
 import { Button } from "../ui/button";
+import lightbulb from "/images/lightbulb.svg";
 import { ImagePlus, X } from "lucide-react";
 import React from "react";
 
@@ -7,47 +8,55 @@ function PostModal({ isOpen, onClose }) {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10">
-      <div className="bg-white p-5 rounded shadow-lg ">
+      <div className="bg-white p-7 rounded-3xl shadow-lg w-2/3">
         <div className="flex justify-between">
-          <div>
-            <img />
-            <h1 className="text-3xl font-bold">post options</h1>
+          <div className="flex mb-6 gap-4">
+            <img src={lightbulb} />
+            <h1 className="text-4xl font-bold ">post options</h1>
           </div>
-          <Button className="rounded-full" variant="outline" onClick={onClose}>
+          <Button
+            className="rounded-full p-0"
+            variant="outline"
+            onClick={onClose}
+          >
             <X className="w-6 h-6" />
           </Button>
         </div>
         <form>
-          <div>
-            <div className="flex flex-col">
-              <label className="font-bold">Post Message</label>
-              <textarea
-                className="border border-gray-300 rounded-lg p-2"
-                placeholder="Type your message here"
-              ></textarea>
-            </div>
-            <p>
-              Post message will give the reviewers more details about your
-              portfolio
-            </p>
-            <div className="flex items-center gap-2 bg-gray-100 border-b-2 border-t-2 border-gray-300 pb-2">
-              <ImagePlus className="w-6 h-6" />
-              <div>
-                <label className="font-bold">Thumbnail</label>
-                <div className="flex gap-2">
-                  <input
-                    className="border border-gray-300 rounded-lg p-2"
-                    placeholder="Choose Image No file chosen"
-                  ></input>
-                  <Button>Submit</Button>
-                </div>
-                <p>Upload an Image</p>
+          <div className="flex gap-4">
+            <div className="w-[60%]">
+              <div className="flex flex-col">
+                <label>Post Message</label>
+                <textarea
+                  className="border border-gray-300 rounded-lg p-2 placeholder-gray-500"
+                  placeholder="Type your message here"
+                ></textarea>
               </div>
+              <p className="border-b-2 border-gray-300 pb-2 text-gray-500">
+                Post message will give the reviewers more details about your
+                portfolio
+              </p>
+              <div className="flex items-center gap-2 bg-gray-100 mt-4 mb-4 rounded-lg p-2">
+                <ImagePlus className="w-6 h-6 mr-2.5 ml-4" />
+                <div>
+                  <label>Thumbnail</label>
+                  <div className="flex gap-2">
+                    <input
+                      className="border border-gray-300 rounded-lg p-2 placeholder-gray-500"
+                      placeholder="Choose Image No file chosen"
+                    ></input>
+                    <Button>Submit</Button>
+                  </div>
+                  <p className="text-gray-500">Upload an Image</p>
+                </div>
+              </div>
+              <p className="border-t-2 border-gray-300 pb-2">
+                --Waiting for final design here--
+              </p>
             </div>
-            <p>--Waiting for final design here--</p>
+            <div className=" bg-gray-400 w-10 h-10"></div>
           </div>
-          <div></div>
-          <div className="flex justify-between">
+          <div className="flex justify-between gap-4">
             <Button variant="secondary">Save Changes</Button>
             <div>
               <Button variant="outline">Cancel</Button>
