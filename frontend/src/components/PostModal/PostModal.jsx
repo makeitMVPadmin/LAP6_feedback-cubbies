65b1ea3a10bc2b4a143898187d4874a6376b7eae
Maite <maitemsabaida@gmail.com>
2025-02-18 14:42:45 -0700

    Insert link input field

--- frontend/src/components/PostModal/PostModal.jsx
+++ frontend/src/components/PostModal/PostModal.jsx
@@ -33,54 +33,49 @@ function PostModal({ isOpen, onClose }) {
                   style={{ fontFamily: "Montserrat, sans-serif" }}
                 />
               </div>
-              <div className="flex items-center border border-[#0F172A] rounded-lg px-3 mt-[41px]">
+              <div className="flex items-center border border-[#0F172A] rounded-lg px-3 mt-[41px] mb-[18px]">
                 <Link2 className="w-4 h-4 rotate-[45deg]" />
                 <input
                   className=" flex-1 rounded-lg p-2 placeholder-gray-500"
                   placeholder="Insert Link"
                 />
               </div>
-              {/* <div className="flex items-center gap-2 bg-gray-100 mt-4 mb-4 rounded-lg p-2">
-                <ImagePlus className="w-6 h-6 mr-2.5 ml-4" />
-                <div>
-                  <label>Thumbnail</label>
-                  <div className="flex gap-2">
-                    <input
-                      className="border border-gray-300 rounded-lg p-2 placeholder-gray-500"
-                      placeholder="Choose Image No file chosen"
-                    ></input>
-                    <Button>Submit</Button>
-                  </div>
-                  <p className="text-gray-500">Upload an Image</p>
-                </div>
-              </div> */}
-              <p className="border-t-2 border-gray-300 pb-2">--TAGS HERE--</p>
+              <p className="border-t border-black pb-2">--TAGS HERE--</p>
             </div>
-            <div
-              className="w-[40%] h-[103px] rounded-[8px] bg-cover bg-center flex flex-col justify-between"
-              style={{ backgroundImage: `url(${placeholder})` }}
-            >
-              <img alt="profile photo" />
-              <p
-                className="p-2"
-                style={{
-                  fontFamily: "Fraunces, serif",
-                  fontSize: "16px",
-                  fontStyle: "normal",
-                  fontWeight: "600",
-                  lineHeight: "24px",
-                  color: "#FFF",
-                }}
+            <div>
+              <div
+                className="w-full h-[103px] rounded-[8px] bg-cover bg-center flex flex-col justify-between"
+                style={{ backgroundImage: `url(${placeholder})` }}
               >
-                My Portfolio
-              </p>
+                <img alt="profile photo" />
+                <p
+                  className="p-2"
+                  style={{
+                    fontFamily: "Fraunces, serif",
+                    fontSize: "16px",
+                    fontStyle: "normal",
+                    fontWeight: "600",
+                    lineHeight: "24px",
+                    color: "#FFF",
+                  }}
+                >
+                  My Portfolio
+                </p>
+              </div>
+              <div className="flex items-center border border-[#0F172A] rounded-lg px-3">
+                <ImagePlus className="w-4 h-4" />
+                <input
+                  className=" flex-1 rounded-lg p-2 placeholder-gray-500"
+                  placeholder="Insert Link"
+                />
+              </div>
             </div>
           </div>
           <div className="flex justify-between">
-            <Button variant="outline" className="border-0">
+            <Button variant="outline" className="border-0 shadow-none">
               Cancel
             </Button>
-            <Button>Publish</Button>
+            <Button className="bg-[#0099ff]">Publish</Button>
           </div>
         </form>
       </div>
