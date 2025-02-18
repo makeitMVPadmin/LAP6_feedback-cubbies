401c64b256c617b920fe71b10af248ff4071b91a
Maite <maitemsabaida@gmail.com>
2025-02-18 13:54:28 -0700

    Final design styles changes. Post message and link only.

--- frontend/src/components/PostModal/PostModal.jsx
+++ frontend/src/components/PostModal/PostModal.jsx
@@ -1,42 +1,56 @@
 import { Button } from "../ui/button";
-import lightbulb from "/images/lightbulb.svg";
-import { ImagePlus, X } from "lucide-react";
+import { ImagePlus, X, Link2 } from "lucide-react";
 import React from "react";
+import { useEffect } from "react";
 
 function PostModal({ isOpen, onClose }) {
   if (!isOpen) return null;
 
+  //   useEffect(() => {
+  //     if (isOpen) {
+  //       document.body.classList.add("overflow-hidden");
+  //     } else {
+  //       document.body.classList.remove("overflow-hidden");
+  //     }
+
+  //     return () => document.body.classList.remove("overflow-hidden");
+  //   }, [isOpen]);
+
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10">
-      <div className="bg-white p-7 rounded-3xl shadow-lg w-2/3">
-        <div className="flex justify-between">
-          <div className="flex mb-6 gap-4">
-            <img src={lightbulb} />
-            <h1 className="text-4xl font-bold ">post options</h1>
-          </div>
-          <Button
-            className="rounded-full p-0"
-            variant="outline"
-            onClick={onClose}
-          >
-            <X className="w-6 h-6" />
-          </Button>
-        </div>
+    <div className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-10">
+      <div className="bg-white p-7 rounded-2xl border-2 border-black shadow-md w-[80%] max-w-[1014px] h-[603px] mt-[145px] overflow-hidden pt-[37px] pl-[62px] pr-[62px] pb-[16px]">
+        <h2
+          className="text-2xl font-bold mb-40px"
+          style={{
+            fontFamily: "Fraunces, serif",
+          }}
+        >
+          Publish a Post
+        </h2>
         <form>
-          <div className="flex gap-4">
+          <div className="flex gap-4 mt-[40px]">
             <div className="w-[60%]">
-              <div className="flex flex-col">
-                <label>Post Message</label>
+              <div className="flex flex-col gap-2">
+                <label
+                  className="font-bold"
+                  style={{ fontFamily: "Montserrat, sans-serif" }}
+                >
+                  Post message
+                </label>
                 <textarea
-                  className="border border-gray-300 rounded-lg p-2 placeholder-gray-500"
-                  placeholder="Type your message here"
-                ></textarea>
+                  className="border border-[#0F172A] rounded-lg px-3 py-2 placeholder-gray-500 h-[80px]"
+                  placeholder="Post message will give the reviewers more details about your portfolio"
+                  style={{ fontFamily: "Montserrat, sans-serif" }}
+                />
               </div>
-              <p className="border-b-2 border-gray-300 pb-2 text-gray-500">
-                Post message will give the reviewers more details about your
-                portfolio
-              </p>
-              <div className="flex items-center gap-2 bg-gray-100 mt-4 mb-4 rounded-lg p-2">
+              <div className="flex items-center border border-[#0F172A] rounded-lg px-3 mt-[41px]">
+                <Link2 className="w-4 h-4 rotate-[45deg]" />
+                <input
+                  className=" flex-1 rounded-lg p-2 placeholder-gray-500"
+                  placeholder="Insert Link"
+                />
+              </div>
+              {/* <div className="flex items-center gap-2 bg-gray-100 mt-4 mb-4 rounded-lg p-2">
                 <ImagePlus className="w-6 h-6 mr-2.5 ml-4" />
                 <div>
                   <label>Thumbnail</label>
@@ -49,10 +63,8 @@ function PostModal({ isOpen, onClose }) {
                   </div>
                   <p className="text-gray-500">Upload an Image</p>
                 </div>
-              </div>
-              <p className="border-t-2 border-gray-300 pb-2">
-                --Waiting for final design here--
-              </p>
+              </div> */}
+              <p className="border-t-2 border-gray-300 pb-2">--TAGS HERE--</p>
             </div>
             <div className=" bg-gray-400 w-10 h-10"></div>
           </div>
