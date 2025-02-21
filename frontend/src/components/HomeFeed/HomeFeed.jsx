27425a071ba6f43162a264f1198509e88b43c20e
jamie ingalls <jamesmaclay@gmail.com>
2025-02-21 15:45:53 -0700

    removed mock data

--- frontend/src/components/HomeFeed/HomeFeed.jsx
+++ frontend/src/components/HomeFeed/HomeFeed.jsx
@@ -1,31 +1,10 @@
 import Post from '../Post/Post.jsx';
 import { Button } from '../ui/button';
 import { Card } from '../ui/card';
-import img1 from '/images/MockImg1.jpg';
-import img2 from '/images/MockImg2.jpg';
-import img3 from '/images/MockImg3.jpg';
-import resume from '/images/MockResume.png';
 import React from 'react';
 
 // loop through the posted portfolios
 function HomeFeed() {
-  // mock data - replace with actual data later
-  let mockPosts = [
-    {
-      id: 1,
-      username: 'johnSmith',
-      title: 'My Portfolio',
-      description: 'Brief description of my portfolio. More details.',
-      images: [img1, img2, img3],
-    },
-    {
-      id: 2,
-      username: 'janeDoe',
-      title: 'My Resume',
-      description: 'My resume, looking for feedback.',
-      images: [resume],
-    },
-  ];
 
   return (
     <section className="grid grid-cols-1 gap-[3.13rem] justify-items-center">
@@ -34,9 +13,9 @@ function HomeFeed() {
           <Button className="bg-[#0099ff] h-[3rem] ">Start Creating A Post</Button>
         </div>
       </Card>
-      {mockPosts.map((post) => (
+      {/* {mockPosts.map((post) => (
         <Post key={post.id} post={post} />
-      ))}
+      ))} */}
     </section>
   );
 }
