8fe66de734d17302e6466b4e22261b4e8baf0dea
jamie ingalls <jamesmaclay@gmail.com>
2025-02-21 15:39:58 -0700

    #

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import PortfolioPage from "../components/Portfolio/Portfolio";
 import PostModal from "../components/PostModal/PostModal";
 import { Button } from "../components/ui/button";
 import HomeFeed from "@/components/HomeFeed/HomeFeed";
-import TopNav from "@/components/TopNav/TopNav";
 import { useState } from "react";
 
 export default function HomePage() {
@@ -12,7 +11,6 @@ export default function HomePage() {
     <div>
       {/* <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page.</p> */}
-      
       <HomeFeed />
       <PortfolioPage />
     </div>
