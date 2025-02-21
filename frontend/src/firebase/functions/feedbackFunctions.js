769bf8ea81c60edbdaa6a10880907fcc6703dbf0
Tanya Vezyr <tetiana.vezyr@gmail.com>
2025-02-21 16:41:05 -0700

    update getFeedback function with fetching username and profile_photo

--- frontend/src/firebase/functions/feedbackFunctions.js
+++ frontend/src/firebase/functions/feedbackFunctions.js
@@ -2,6 +2,7 @@ import { db } from "../firebase.js";
 import {
   collection,
   doc,
+  getDoc,
   getDocs,
   addDoc,
   updateDoc,
@@ -17,10 +18,24 @@ const getPortfolioFeedback = async (portfolioId) => {
   );
   try {
     const feedbackSnapshot = await getDocs(feedbackQuery);
-    const feedbackList = feedbackSnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
+    const feedbackList = [];
+
+    for (const docSnapshot of feedbackSnapshot.docs) {
+      const feedbackData = docSnapshot.data();
+      const userId = feedbackData.user_id;
+
+      const userDoc = await getDoc(doc(db, "users", userId));
+      const userData = userDoc.exists() ? userDoc.data() : { user_name: "Unknown", user_profilePicture: "" };
+      console.log(userData);
+
+      feedbackList.push({
+        id: doc.id,
+        ...feedbackData,
+        user_name: userData.username,
+        user_profilePicture: userData.profile_photo,
+      });
+    }
+
     return feedbackList;
   } catch (err) {
     console.error("Error getting feedback: ", err);
