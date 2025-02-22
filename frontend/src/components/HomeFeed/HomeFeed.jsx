9a68a06cf269a68b5ee4deb19f85af2b564c05c9
jamie ingalls <jamesmaclay@gmail.com>
2025-02-21 17:54:46 -0700

    Create Post Button, Tags Button, & Avatar Icon added and styled

--- frontend/src/components/HomeFeed/HomeFeed.jsx
+++ frontend/src/components/HomeFeed/HomeFeed.jsx
@@ -1,12 +1,9 @@
-import Post from "../Post/Post.jsx";
-import PostModal from "../PostModal/PostModal.jsx";
-import { Button } from "../ui/button";
-import { Card } from "../ui/card";
-import img1 from "/images/MockImg1.jpg";
-import img2 from "/images/MockImg2.jpg";
-import img3 from "/images/MockImg3.jpg";
-import resume from "/images/MockResume.png";
-import React, { useState } from "react";
+import Post from '../Post/Post.jsx';
+import PostModal from '../PostModal/PostModal.jsx';
+import { Button } from '../ui/button';
+import { Card } from '../ui/card';
+import ChevronDown from '../ui/chevron-down';
+import React, { useState } from 'react';
 
 // loop through the posted portfolios
 function HomeFeed() {
@@ -16,31 +13,25 @@ function HomeFeed() {
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
 
-  // mock data - replace with actual data later
-
-  let mockPosts = [
-    {
-      id: 1,
-      username: "johnSmith",
-      title: "My Portfolio",
-      description: "Brief description of my portfolio. More details.",
-      images: [img1, img2, img3],
-    },
-    {
-      id: 2,
-      username: "janeDoe",
-      title: "My Resume",
-      description: "My resume, looking for feedback.",
-      images: [resume],
-    },
-  ];
-
   return (
-    <section className="grid grid-cols-1 gap-[3.13rem] justify-items-center">
-      <Card className="w-[29.4375rem] p-[0.5rem] border-y-1 border-x-2 rounded-[5px] #28363F gap-[o.625rem]">
-        <div className="w-[25.93575rem] p-x-[0.5rem] flex flex-row justify-center">
-          <Button className="bg-[#0099ff] h-[3rem] " onClick={handleOpenModal}>
+    <div className="grid grid-cols-1 gap-[3.13rem] justify-items-center">
+      <Card className="h-24 p-6 rounded-lg border-l border-r-2 border-t border-b-2 border-[#28363f] justify-start items-start gap-6 inline-flex overflow-hidden">
+        <div className="h-12 px-6 justify-start items-center gap-16 inline-flex">
+          <img
+            className="w-9 h-9 rounded-full"
+            src="https://s3-alpha-sig.figma.com/img/8757/ea43/56d61cf7bb51515bf9da9c2f34ea9d23?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lIZuF74W~vHigZ4RccPfTTsvUcjXZI3QYmLos9h-AKERaZtZd71LC0oAptDzu4fUExn46v91WJIk71pklm1W62ZykvwPKu~FZUvD1ylZ1btHfQqlkNVWQh~DG9-pH3ZG7YJhRRGBEdOtJF1N-PwSFIrtAmufTs62Zs5dT6VHlGzzCMMuBsh0tE1~WRFLq9rhXUHJ7scXLYDG06xd9q9H7oDd3qDPr0AmFfwFgrbI1haBvV2nLSDGB8B6TjIDLHns0w3R0xG9Oruuog81fq3ZkVetKIap~v8PvGGo8J5EZ84RHkcH9nj6~Jd5aCNsvDyuHqZkXDcXPaqIl2jgkVkMfA__"
+          />
+
+          <Button
+            className="h-12 pl-4 pr-5 py-2.5 bg-[#0264d4] hover:bg-[#024a9b] rounded-[10px] shadow-sm justify-center items-center gap-2 inline-flex"
+            onClick={handleOpenModal}>
             Start Creating A Post
+          </Button>
+          <Button
+            variant="outline"
+            className="h-12 pl-4 pr-5 bg-[#ffd22f] hover:bg-[#e6b800] rounded-[10px] justify-center items-center gap-2 inline-flex text-center text-[#28363f] text-xl font-medium leading-7 ">
+            <ChevronDown className="w-3 h-3" />
+            Tags
           </Button>
         </div>
       </Card>
@@ -48,7 +39,7 @@ function HomeFeed() {
       {/* {mockPosts.map((post) => (
         <Post key={post.id} post={post} />
       ))} */}
-    </section>
+    </div>
   );
 }
 
