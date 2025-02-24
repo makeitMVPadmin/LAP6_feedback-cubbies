1f06e796ee93521309b9cc5ed7d0619bec7fab1b
Tanya Vezyr <tetiana.vezyr@gmail.com>
2025-02-24 11:25:50 -0700

    add basic styling

--- frontend/src/components/Feedback/Feedback.jsx
+++ frontend/src/components/Feedback/Feedback.jsx
@@ -1,19 +1,19 @@
-import { useState, useEffect } from "react";
+import avatar from "../../assets/avatar.svg";
+import sendIcon from "../../assets/send.svg";
 import {
   getPortfolioFeedback,
   createFeedback,
   updateFeedback,
   deleteFeedback,
 } from "../../firebase/functions/feedbackFunctions";
-
+import { useState, useEffect } from "react";
 
 const Feedback = () => {
   const [feedbackList, setFeedbackList] = useState([]);
-  const [portfolioId, setPortfolioId] = useState("CqHPx3kVvGMNZFu5pbUB");
+  const [portfolioId, setPortfolioId] = useState("U3LLN8Jtc4JYgdYICZi6");
   const [newComment, setNewComment] = useState("");
   const [editingFeedbackId, setEditingFeedbackId] = useState(null);
   const [updatedComments, setUpdatedComments] = useState({});
-
 
   const retrivePortfolioFeedback = async () => {
     const feedback = await getPortfolioFeedback(portfolioId);
@@ -37,7 +37,6 @@ const Feedback = () => {
     setNewComment("");
   };
 
- 
   const handleUpdateFeedback = async (feedbackId) => {
     const updatedComment = updatedComments[feedbackId];
     if (updatedComment.trim() !== "") {
@@ -93,51 +92,66 @@ const Feedback = () => {
 
   return (
     <div>
-      <h2>Feedbacks for Portfolio {portfolioId}</h2>
-      <ul>
-        {feedbackList.map((feedback) => (
-          <li key={feedback.id}>
-            {editingFeedbackId === feedback.id ? (
+      <section className="grid grid-cols-1 gap-[3.13rem] pl-[3.13rem] justify-items-center">
+        <div className="flex flex-col items-start gap-[1.5rem] p-[1.5rem] w-[47.125rem] rounded-lg border-t border-r-2 border-b-2 border-l border-gray-600">
+          {feedbackList.map((feedback) => (
+            <div
+              key={feedback.id}
+              className="inline-flex items-start gap-[0.25rem] px-[0.75rem] self-stretch"
+            >
               <div>
-                <input
-                  type="text"
-                  value={updatedComments[feedback.id] || feedback.comment}
-                  onChange={(e) =>
-                    setUpdatedComments((prev) => ({
-                      ...prev,
-                      [feedback.id]: e.target.value,
-                    }))
-                  }
-                />
-                <button onClick={() => handleUpdateFeedback(feedback.id)}>
-                  Save
-                </button>
-                <button onClick={handleCancelEdit}>Cancel</button>
+                <img src={avatar} alt="user avatar" />
+                {/* <img src={feedback.profilePhoto || avatar} alt="user avatar" /> */}
               </div>
-            ) : (
               <div>
-                <p onClick={() => handleEditClick(feedback.id)}>
-                  {feedback.comment}
-                </p>
-              </div>
-            )}
+                <p className="font-bold">@{feedback.username}</p>
+                {editingFeedbackId === feedback.id ? (
+                  <div>
+                    <input
+                      type="text"
+                      value={updatedComments[feedback.id] || feedback.comment}
+                      onChange={(e) =>
+                        setUpdatedComments((prev) => ({
+                          ...prev,
+                          [feedback.id]: e.target.value,
+                        }))
+                      }
+                    />
+                    <button onClick={() => handleUpdateFeedback(feedback.id)}>
+                      Save
+                    </button>
+                    <button onClick={handleCancelEdit}>Cancel</button>
+                  </div>
+                ) : (
+                  <div>
+                    <p onClick={() => handleEditClick(feedback.id)}>
+                      {feedback.comment}
+                    </p>
+                  </div>
+                )}
 
-            <button onClick={() => handleDeleteFeedback(feedback.id)}>
-              Delete
+                {/* <button onClick={() => handleDeleteFeedback(feedback.id)}>
+                Delete
+              </button> */}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="w-[47.125rem]">
+          <div className="flex items-center gap-[0.625rem] w-[36.875rem] h-[2.5rem] px-[1px] rounded-lg border-t border-r-2 border-b-2 border-l border-gray-600 bg-white">
+            <input
+              type="text"
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              placeholder="Add comment..."
+              className="flex-grow p-0 outline-none bg-transparent"
+            />
+            <button onClick={handleCreateFeedback}>
+              <img src={sendIcon} className="w-6 h-6 text-gray-800" />
             </button>
-          </li>
-        ))}
-      </ul>
-
-      <div>
-        <input
-          type="text"
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-          placeholder="Write a feedback"
-        />
-        <button onClick={handleCreateFeedback}>Submit Feedback</button>
-      </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
