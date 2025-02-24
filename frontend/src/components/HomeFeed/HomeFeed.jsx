2df788cfacaf5492aa013663355edac272c82a28
Darius Fang <dariusfang1@gmail.com>
2025-02-24 16:48:16 -0700

    remnoved home feed function

--- frontend/src/components/HomeFeed/HomeFeed.jsx
+++ frontend/src/components/HomeFeed/HomeFeed.jsx
@@ -1,9 +1,6 @@
 import CreatePost from "../CreatePost/CreatePost.jsx";
 import Portfolio from "../Portfolio/Portfolio.jsx";
 
-function HomeFeed() {
-
-
 function HomeFeed({ currentUser }) {
   return (
     <div className="grid grid-cols-1 gap-[3.13rem] justify-items-center">
