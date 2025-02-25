a14d9243489d9a6832150d080164ef93017c6434
Darius Fang <dariusfang1@gmail.com>
2025-02-25 15:13:58 -0700

    fixed top nav, not referencing after navigation context was added

--- frontend/src/context/NavigationContext.jsx
+++ frontend/src/context/NavigationContext.jsx
@@ -6,6 +6,7 @@ export const NavigationProvider = ({ children }) => {
   const [currentPage, setCurrentPage] = useState("home");
   const [currentNavState, setCurrentNavState] = useState("home");
   const [portfolioId, setPortfolioId] = useState(null);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   // Navigate to Portfolio Details Page
   const goToProfileDetails = (id) => {
@@ -20,9 +21,25 @@ export const NavigationProvider = ({ children }) => {
     setCurrentNavState("home");
   };
 
+  // Open and Close Notification Drawer
+  const openDrawer = () => setIsDrawerOpen(true);
+  const closeDrawer = () => setIsDrawerOpen(false);
+  const toggleDrawer = () => setIsDrawerOpen((prev) => !prev);
+
   return (
     <NavigationContext.Provider
-      value={{ currentPage, currentNavState, goToProfileDetails, goToHome, portfolioId, setPortfolioId }}
+      value={{
+        currentPage,
+        currentNavState,
+        goToProfileDetails,
+        goToHome,
+        portfolioId,
+        setPortfolioId,
+        isDrawerOpen,
+        openDrawer,
+        closeDrawer,
+        toggleDrawer,
+      }}
     >
       {children}
     </NavigationContext.Provider>
