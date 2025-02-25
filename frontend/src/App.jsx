9a15eb9a8a52948557f1b7b3657cf8c7afd6d16b
Darius Fang <dariusfang1@gmail.com>
2025-02-25 15:58:31 -0700

    added userContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,17 @@
 import NotificationDrawer from "./components/NotificationDrawer/NotificationDrawer";
 import { NavigationProvider, useNavigation } from "./context/NavigationContext";
-import { fetchUsersByIds, emptyUser } from "./firebase/functions/fetchUsers";
+import { UserProvider, useUser } from "./context/UserContext";
 import HomePage from "./pages/HomePage";
 import PortfolioDetailsPage from "./pages/PortfolioDetailsPage";
 import TopNav from "@/components/TopNav/TopNav";
-import { useState, useEffect } from "react";
 import "./App.css";
+import { emptyUser } from "./firebase/functions/fetchUsers";
 import { Toaster } from "sonner";
 
 function AppContent() {
-  const {
-    currentPage,
-    goToProfileDetails,
-    isDrawerOpen,
-    openDrawer,
-    closeDrawer,
-  } = useNavigation();
-  const [usersList, setUsersList] = useState([]);
-  const [currentUser, setCurrentUser] = useState(null);
-
-  useEffect(() => {
-    const userIds = [
-      "01x5EDdS2TlLNMPLkiPT",
-      "03E7eZ9ODHyBiguPGXtw",
-      "0U4AlgjUcfgyC23raMTo",
-    ];
-
-    fetchUsersByIds(userIds).then((users) => {
-      setUsersList(users);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (usersList.length > 0 && !currentUser) {
-      setCurrentUser(usersList[0]);
-      console.log("User set:", usersList[0]);
-    }
-  }, [usersList]);
+  const { currentPage, goToProfileDetails, isDrawerOpen, closeDrawer } =
+    useNavigation();
+  const { usersList, currentUser, handleUserLogin } = useUser();
 
   const renderPage = () => {
     switch (currentPage) {
@@ -53,11 +28,12 @@ function AppContent() {
     <div className="min-h-screen flex flex-col">
       <header className="bg-white shadow-md p-4">
         <TopNav
+          setCurrentPage={goToProfileDetails}
           currentPage={currentPage}
           notificationCount="999"
           currentUser={currentUser || emptyUser}
           usersList={usersList}
-          openDrawer={openDrawer} // Pass function to TopNav
+          handleUserLogin={handleUserLogin} // Pass login function
         />
       </header>
       <main className="flex-grow p-4">{renderPage()}</main>
@@ -66,7 +42,7 @@ function AppContent() {
       <NotificationDrawer
         currentUser={currentUser || emptyUser}
         isOpen={isDrawerOpen}
-        onClose={closeDrawer}
+        onClose={() => closeDrawer()}
       />
     </div>
   );
@@ -76,7 +52,11 @@ function App() {
   return (
     <>
       <NavigationProvider>
-        <AppContent />
+        <UserProvider>
+          {" "}
+          {/* Wrap AppContent with UserProvider */}
+          <AppContent />
+        </UserProvider>
       </NavigationProvider>
       <div className="min-h-screen flex flex-col">
         <Toaster position="bottom-right" richColors />
