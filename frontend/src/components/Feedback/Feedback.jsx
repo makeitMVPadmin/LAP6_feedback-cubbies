fa49b42622746ea50ec161a432f695818400678a
Tanya Vezyr <tetiana.vezyr@gmail.com>
2025-02-28 01:02:05 -0700

    code refactoring

--- frontend/src/components/Feedback/Feedback.jsx
+++ frontend/src/components/Feedback/Feedback.jsx
@@ -40,36 +40,42 @@ const Feedback = ({ portfolioId }) => {
   }, [portfolioId]);
 
   return (
-    <div className="w-full w-[754px]">
-      <div className=" pt-6 pl-6 pr-6 mt-[64px] bg-white rounded-lg border-l border-r-2 border-t border-b-2 border-[#28363f] flex flex-col justify-start items-start overflow-hidden">
+    <div className="w-full max-w-[754px] mx-auto">
+      <div className="pt-6 px-6 mt-[64px] bg-white rounded-lg border border-[#28363f] flex flex-col justify-start items-start overflow-hidden">
         {feedbackList.length === 0 ? (
-          <p className="text-black text-sm font-['Montserrat'] font-normal leading-[20px] custom-font-settings mb-6">There are no comments yet.</p>
+          <p className="text-black text-sm font-['Montserrat'] font-normal leading-[20px] custom-font-settings mb-6">
+            There are no comments yet.
+          </p>
         ) : (
           feedbackList.map((feedback) => (
             <div
               key={feedback.id}
               className="flex items-start px-3 mb-6 self-stretch"
             >
-              <div className="justify-center items-center pr-4" >
-                <img src={avatar} alt="user avatar" className="w-9 h-9"/>
-                {/* <img src={feedback.profilePhoto || avatar} alt="user avatar" /> */}
+              <div className="pr-4">
+                <img src={avatar} alt="user avatar" className="w-9 h-9" />
               </div>
               <div className="flex flex-col items-start">
-                <p className="text-black text-sm font-['Montserrat'] font-semibold leading-[20px] custom-font-settings">@{feedback.username}</p>
-                <p className="text-black text-sm font-['Montserrat'] font-normal leading-[20px] custom-font-settings">{feedback.comment}</p>
+                <p className="text-black text-sm font-['Montserrat'] font-semibold leading-[20px] custom-font-settings">
+                  @{feedback.username}
+                </p>
+                <p className="text-black text-sm font-['Montserrat'] font-normal leading-[20px] custom-font-settings">
+                  {feedback.comment}
+                </p>
               </div>
             </div>
           ))
         )}
       </div>
-      <div className="w-full w-[754px] pt-[64px]">
-        <div className="flex w-[590px] h-[40px] items-center gap-[10px] flex-shrink-0 bg-white rounded-lg border-l border-r-2 border-t border-b-2 border-[#28363f]">
+
+      <div className="w-full pt-[64px]">
+        <div className="flex w-full max-w-[590px] h-[40px] items-center gap-[10px] bg-white rounded-lg border border-[#28363f]">
           <input
             type="text"
             value={newComment}
             onChange={(e) => setNewComment(e.target.value)}
             placeholder="Add comment..."
-            className="w-full h-full px-3 py-1 text-sm font-['Montserrat'] font-normal leading-[20px] placeholder:text-[#80909A] placeholder:font-['Montserrat'] placeholder:text-sm placeholder:font-normal placeholder:leading-[20px] placeholder: custom-font-settings focus:outline-none"
+            className="w-full h-full px-3 py-1 text-sm font-['Montserrat'] font-normal leading-[20px] placeholder:text-[#80909A] placeholder:font-['Montserrat'] placeholder:text-sm placeholder:font-normal placeholder:leading-[20px] focus:outline-none"
           />
           <button onClick={handleCreateFeedback} className="pr-3">
             <img src={sendIcon} className="w-6 h-6" />
